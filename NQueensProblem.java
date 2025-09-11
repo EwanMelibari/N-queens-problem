5750f910a3d622d60991577d437a9f86df6804c7
Ewan Melibari <ewanhasan2004@gmail.com>
2025-09-11 12:40:25 +0300

    Implementing the solution I

--- NQueensProblem.java
+++ NQueensProblem.java
@@ -1,3 +1,40 @@
 public class NQueensProblem {
+
+    private int[][] chessBoard;
+    private int numOfQueens;
+
+    public NQueensProblem(int numOfQueens){
+        this.numOfQueens = numOfQueens;
+        this.chessBoard = new int[numOfQueens][numOfQueens];
+    }
+
+    public void solve(){
+        if(setQueens(0)){
+            printQueens();
+        }
+    }
+
+    private boolean setQueens(int colIndex){
+        return false;
+    }
+
+    private boolean isPlaceValid(int rowIndexm, int columnIndex){
+        return false;
+    }
+
+    private void printQueens(){
+        for(int i = 0; i < chessBoard.length; i++){
+            for(int j = 0; j < chessBoard.length; j++){
+
+                if(chessBoard[i][j] == 1){
+                    System.out.print(" ♕ ");
+                }else{
+                    System.out.println(" - ");
+                }
+
+                System.out.println();                
+            }
+
+    }
     
 }
